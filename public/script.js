402a2883faf4d433ebfa586be7e94c7839cc85ce
EcceAgnusDei <mondo.antoine@yahoo.fr>
2025-10-20 15:08:14 +0200

    + gestion de navigation

--- public/script.js
+++ public/script.js
@@ -1,129 +1,46 @@
 import { Grid } from "./game.js";
 
-function likeButton(grid, userId) {
-  let state = grid.likes.includes(userId + "") ? "unlike" : "like";
+let userId;
 
-  const container = document.createElement("div");
-
-  const button = document.createElement("button");
-  button.setAttribute("class", "loginonly"); ////!!!!!optimiser le changement d'état
-  button.style.display = userId ? "block" : "none";
-  button.innerText = state;
-  button.onclick = async () => {
-    if (userId) {
-      if (state === "like") {
-        try {
-          await like();
-        } catch (error) {
-          console.log("Erreur lors du like:", error.message);
-        }
-      } else if (state === "unlike") {
-        try {
-          await unlike();
-        } catch (error) {
-          console.log("Erreur lors du unlike:", error.message);
-        }
-      }
-      button.innerHTML = state;
+function changeState(state) {
+  const logoutOnly = document.getElementsByClassName("logoutonly");
+  const loginOnly = document.getElementsByClassName("loginonly");
+  for (let element of logoutOnly) {
+    if (state === "logout") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
     }
-  };
-
-  const buttonPlaceholder = document.createElement("div");
-  buttonPlaceholder.innerText = "likes";
-  buttonPlaceholder.setAttribute("class", "logoutonly");
-  buttonPlaceholder.style.display = userId ? "none" : "block";
-
-  container.appendChild(button);
-  container.appendChild(buttonPlaceholder);
-
-  async function like() {
-    const response = await fetch(
-      `https://localhost:3000/like/${grid.grid_id}`,
-      {
-        method: "PUT",
-      }
-    );
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(`Erreur HTTP : ${errorData.error || response.status}`);
-    }
-
-    const result = await response.json();
-    console.log(result.data);
-    state = "unlike";
   }
-  async function unlike() {
-    const response = await fetch(
-      `https://localhost:3000/unlike/${grid.grid_id}`,
-      {
-        method: "PUT",
-      }
-    );
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(`Erreur HTTP : ${errorData.error || response.status}`);
+  for (let element of loginOnly) {
+    if (state === "login") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
     }
-
-    const result = await response.json();
-    console.log(result.data);
-    state = "like";
   }
-
-  return container;
 }
 
 function handleLogin() {
-  const loginForm = document.getElementById("loginform");
-  loginForm.setAttribute("class", "logoutonly");
-
-  const logoutButton = document.getElementById("logoutbutton");
-  logoutButton.setAttribute("class", "loginonly");
-  logoutButton.onclick = async () => {
-    try {
-      const result = await logout();
-      console.log(result.message);
-      if (result.success) {
-        changeState("logout");
+  (async function handleLogout() {
+    const logoutButton = document.getElementById("logoutbutton");
+    logoutButton.onclick = async () => {
+      try {
+        const resp = await fetch(`https://localhost:3000/logout`, {
+          method: "POST",
+          credentials: "include",
+        });
+        const result = await resp.json();
+        console.log(result.message);
+        if (result.success) {
+          changeState("logout");
+        }
+        userId = undefined;
+      } catch (error) {
+        console.log("Erreur lors de la déconnexion: ", error.message);
       }
-      userId = undefined;
-    } catch (error) {
-      console.log("Erreur lors de la déconnexion: ", error.message);
-    }
-  };
-
-  const saveButton = document.getElementById("savebutton");
-  saveButton.setAttribute("class", "loginonly");
-
-  let userId;
-  changeState("logout");
-
-  function changeState(state) {
-    const logoutOnly = document.getElementsByClassName("logoutonly");
-    const loginOnly = document.getElementsByClassName("loginonly");
-    for (let element of logoutOnly) {
-      if (state === "logout") {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
-    }
-    for (let element of loginOnly) {
-      if (state === "login") {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
-    }
-  }
-
-  async function logout() {
-    const resp = await fetch(`https://localhost:3000/logout`, {
-      method: "POST",
-      credentials: "include",
-    });
-    const result = await resp.json();
-    return result;
-  }
+    };
+  })();
 
   (async function authentifyAtLoad() {
     try {
@@ -150,7 +67,9 @@ function handleLogin() {
     }
   })();
 
-  (function handleLogin() {
+  (function handleLoginForm() {
+    const loginForm = document.getElementById("loginform");
+    loginForm.style.display = "none";
     const emailInput = document.getElementById("loginemail");
     const passwordInput = document.getElementById("loginpassword");
 
@@ -172,6 +91,7 @@ function handleLogin() {
 
         if (result.success) {
           changeState("login");
+          loginForm.style.display = "none";
           userId = result.data.userId;
         }
 
@@ -182,8 +102,9 @@ function handleLogin() {
     };
   })();
 
-  (function handleSignin() {
+  (function handleSigninForm() {
     const signinForm = document.getElementById("signinform");
+    signinForm.style.display = "none";
     const signinEmailInput = document.getElementById("signinemail");
     const signinPasswordInput = document.getElementById("signinpassword");
     const signinNameInput = document.getElementById("signinname");
@@ -216,56 +137,8 @@ function handleLogin() {
         }
       }
     };
-  })(); /////!!!!!!!! gérer la navbar
-
-  (function handleLoad() {
-    const loadAllButton = document.getElementById("loadallbutton");
-    loadAllButton.onclick = () => fetchGrids("grids/");
-
-    const loadUserGridsButton = document.getElementById("loadusergridbutton");
-    loadUserGridsButton.setAttribute("class", "loginonly");
-    loadUserGridsButton.onclick = () => fetchGrids("mygrids/");
-
-    async function fetchGrids(path) {
-      try {
-        const response = await fetch(`https://localhost:3000/${path}`, {
-          method: "GET",
-          credentials: "include",
-        });
-        const result = await response.json();
-        result.data.grids.forEach((grid, index) => {
-          console.log(grid);
-          const gridContainer = document.createElement("div");
-          const likesContainer = document.createElement("div");
-          const likesNumber = document.createElement("div");
-          document.getElementById("gridsdisplayer").appendChild(gridContainer);
-          const fetchedGrid = new Grid(
-            false,
-            gridContainer,
-            `displayedgrid${index}`
-          );
-          fetchedGrid.mount();
-          fetchedGrid.loadGrid(grid.alive_cells, grid.grid_size);
-          gridContainer.appendChild(likesContainer);
-          likesContainer.style.display = "flex";
-          likesContainer.appendChild(likesNumber);
-          likesNumber.innerText = grid.likes.length;
-          likesContainer.appendChild(likeButton(grid, userId)); ///!!! voir si on peut pas gérer la vérification coté client
-          /*const likesText = document.createElement("div");
-          likesText.setAttribute("class", "logoutonly");
-          likesText.innerText = "likes";
-          likesContainer.appendChild(likesText);*/
-        });
-      } catch (error) {
-        console.log(
-          "Erreur lors du chargement des grilles: ",
-          error.message,
-          error.stack
-        );
-      }
-    }
-  })(); /////!!!!!!!! gérer le changement de page et le scroll
-} ////!!!!gérer le dashboard
+  })();
+}
 
 function handleGame() {
   const gridContainer = document.getElementById("gridcontainer");
@@ -408,7 +281,193 @@ function handleGame() {
   })();
 }
 
+function handleInterface() {
+  (function handleMutation() {
+    const login = document.getElementById("login");
+    login.setAttribute("class", "logoutonly");
+
+    const signin = document.getElementById("signin");
+    signin.setAttribute("class", "logoutonly");
+
+    const favorites = document.getElementById("favorites");
+    favorites.setAttribute("class", "loginonly");
+
+    const myCreations = document.getElementById("loadusergridbutton");
+    myCreations.setAttribute("class", "loginonly");
+
+    const logoutButton = document.getElementById("logoutbutton");
+    logoutButton.setAttribute("class", "loginonly");
+
+    const saveButton = document.getElementById("savebutton");
+    saveButton.setAttribute("class", "loginonly");
+
+    changeState("logout"); //initialisation chargement de la page
+  })();
+
+  (function handleNavigation() {
+    clearPage(); //initialisation au chargement de la page
+
+    (function handleButtons() {
+      const homePage = document.getElementById("gridsdisplayerpage");
+      homePage.style.display = "block";
+      fetchGrids("grids/");
+
+      const homeButton = document.getElementById("home");
+      homeButton.onclick = () => {
+        clearPage();
+        homePage.style.display = "block";
+        fetchGrids("grids/");
+      };
+
+      const loadUserGridsButton = document.getElementById("loadusergridbutton");
+      loadUserGridsButton.onclick = () => fetchGrids("mygrids/");
+
+      const signin = document.getElementById("signin");
+      signin.onclick = () => {
+        document.getElementById("signinform").style.display = "block";
+      };
+
+      const login = document.getElementById("login");
+      login.onclick = () => {
+        document.getElementById("loginform").style.display = "block";
+      };
+
+      const create = document.getElementById("create");
+      create.onclick = () => {
+        clearPage();
+        document.getElementById("createpage").style.display = "block";
+      };
+    })();
+
+    function clearPage() {
+      const page = document.getElementById("page");
+      const children = Array.from(page.children);
+      children.forEach((element) => {
+        element.style.display = "none";
+      });
+    }
+
+    async function fetchGrids(path) {
+      const gridsDisplayer = document.getElementById("gridsdisplayer");
+      const children = Array.from(gridsDisplayer.children);
+      children.forEach((gridContainer) => gridContainer.remove());
+
+      try {
+        const response = await fetch(`https://localhost:3000/${path}`, {
+          method: "GET",
+          credentials: "include",
+        });
+        const result = await response.json();
+        result.data.grids.forEach((grid, index) => {
+          const gridContainer = document.createElement("div");
+          const likesContainer = document.createElement("div");
+          const likesNumber = document.createElement("div");
+          gridsDisplayer.appendChild(gridContainer);
+          const fetchedGrid = new Grid(
+            false,
+            gridContainer,
+            `displayedgrid${index}`
+          );
+          fetchedGrid.mount();
+          fetchedGrid.loadGrid(grid.alive_cells, grid.grid_size);
+          gridContainer.appendChild(likesContainer);
+          likesContainer.style.display = "flex";
+          likesContainer.appendChild(likesNumber);
+          likesNumber.innerText = grid.likes.length;
+          likesContainer.appendChild(likeButton(grid));
+        });
+      } catch (error) {
+        console.log(
+          "Erreur lors du chargement des grilles: ",
+          error.message,
+          error.stack
+        );
+      }
+
+      function likeButton(grid) {
+        let state = grid.likes.includes(userId + "") ? "unlike" : "like";
+
+        const container = document.createElement("div");
+
+        const button = document.createElement("button");
+        button.setAttribute("class", "loginonly");
+        button.style.display = userId ? "block" : "none";
+        button.innerText = state;
+        button.onclick = async () => {
+          if (userId) {
+            if (state === "like") {
+              try {
+                await like();
+              } catch (error) {
+                console.log("Erreur lors du like:", error.message);
+              }
+            } else if (state === "unlike") {
+              try {
+                await unlike();
+              } catch (error) {
+                console.log("Erreur lors du unlike:", error.message);
+              }
+            }
+            button.innerHTML = state;
+          }
+        };
+
+        const buttonPlaceholder = document.createElement("div");
+        buttonPlaceholder.innerText = "likes";
+        buttonPlaceholder.setAttribute("class", "logoutonly");
+        buttonPlaceholder.style.display = userId ? "none" : "block";
+
+        container.appendChild(button);
+        container.appendChild(buttonPlaceholder);
+
+        async function like() {
+          const response = await fetch(
+            `https://localhost:3000/like/${grid.grid_id}`,
+            {
+              method: "PUT",
+            }
+          );
+          if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(
+              `Erreur HTTP : ${errorData.error || response.status}`
+            );
+          }
+
+          const result = await response.json();
+          console.log(result.data);
+          state = "unlike";
+        }
+        async function unlike() {
+          const response = await fetch(
+            `https://localhost:3000/unlike/${grid.grid_id}`,
+            {
+              method: "PUT",
+            }
+          );
+          if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(
+              `Erreur HTTP : ${errorData.error || response.status}`
+            );
+          }
+
+          const result = await response.json();
+          console.log(result.data);
+          state = "like";
+        }
+
+        return container;
+      }
+    }
+  })();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   handleLogin();
   handleGame();
+  handleInterface();
 });
+
+/////!!!!!!!! gérer le changement de page et le scroll
+////!!!!gérer les fonctionnalité utilisateurs
